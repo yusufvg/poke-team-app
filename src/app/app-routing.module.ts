76a9ef890b7190556427f978ab03bea131aab050
Yusuf Van Gieson <yusufvg98@gmail.com>
2021-11-27 17:33:42 -0800

    added routing between edit and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoveragePanelComponent } from './components/coverage-panel/coverage-panel.component';
+import { EditTeamPanelComponent } from './components/edit-team-panel/edit-team-panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoveragePanelComponent },
+  { path: 'edit', component: EditTeamPanelComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
