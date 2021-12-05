12c804526cd87fc5ada12c9983fe12eb67bd3590
Yusuf Van Gieson <yusufvg98@gmail.com>
2021-12-05 14:39:30 -0800

    changed coverage layout, row for each occ tier

--- src/app/components/coverage-card/coverage-card.component.ts
+++ src/app/components/coverage-card/coverage-card.component.ts
@@ -11,19 +11,15 @@ export class CoverageCardComponent implements OnInit {
   @Input() matchup!: Matchup;
   color: string = '#000';
   type: string = '';
-  occ: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {
-    console.log(this.matchup);
     this.color =
       TYPES.find((t) => t.type == this.matchup.target)?.color ?? this.color;
 
     this.type =
       this.matchup.target.charAt(0).toLocaleUpperCase() +
       this.matchup.target.slice(1);
-
-    this.occ = this.matchup.occurences ?? 1;
   }
 }
--- src/app/components/coverage-panel/coverage-panel.component.ts
+++ src/app/components/coverage-panel/coverage-panel.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { Matchup } from 'src/app/interfaces/Matchup';
 import { Report } from 'src/app/interfaces/Report';
 import { TeamService } from 'src/app/services/team.service';
+
+interface OccList {
+  occ: number;
+  list: Matchup[];
+  color?: string;
+}
 
 @Component({
   selector: 'app-coverage-panel',
@@ -9,10 +17,38 @@ import { TeamService } from 'src/app/services/team.service';
 })
 export class CoveragePanelComponent implements OnInit {
   report!: Report;
+  offense: OccList[] = [];
+  defense: OccList[] = [];
 
   constructor(private teamService: TeamService) {}
 
   ngOnInit(): void {
     this.teamService.getTeamReport().subscribe((r) => (this.report = r));
+
+    this.mapOccurences(this.report.offense, this.offense);
+    this.mapOccurences(this.report.defense, this.defense);
+  }
+
+  mapOccurences(source: Matchup[], target: OccList[]): void {
+    source.map((m) => {
+      let countMatch = target.find((o) => o.occ == (m.occurences ?? 1));
+      if (countMatch) {
+        countMatch.list.push(m);
+      } else {
+        target.push({ occ: m.occurences ?? 1, list: [m] });
+      }
+    });
+
+    if (target.length > 1) {
+      let inc = (255 - 120) / (target.length - 1);
+      let [r, g, b] = [120, 0, 0];
+
+      target.forEach((o) => {
+        o.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        [r, g, b] = [r + inc, g + inc, b + inc];
+      });
+    } else {
+      target[0].color = 'rgb(255,135,135)';
+    }
   }
 }
